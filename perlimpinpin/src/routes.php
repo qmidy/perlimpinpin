23ca2e7e9927e6d014be393f81d6ec16e93da5d5
Quentin Midy <q.midy@lectra.com>
2017-10-23 14:07:39 +0200

    Calcul de la date

--- perlimpinpin/src/routes.php
+++ perlimpinpin/src/routes.php
@@ -6,19 +6,46 @@ use Slim\Http\Response;
 include_once 'Service\DataService.php';
 
 // Routes
+$app->get('/[{date}]', function (Request $request, Response $response, array $args) {
+    // la date doit être au format yyyy-mm-dd
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Obtention de la date du jour
-    $today = getdate();
+    if(array_key_exists("date", $args) && validateDate($args["date"]))
+    {
+    	$date = new DateTime($args["date"]);
+    }
+    else
+    {
+    	// Si la date n'est pas initialisée ou n'est pas valide, on fixe à aujourd'hui
+    	$today = getdate();
+    	$date = new DateTime($today["year"]."-".$today["mon"]."-".$today["mday"]);
+    }
+
+    $args["date"] = $date->format("Y-m-d");
+
+    // On calcule les dates des jours suivants et précédents
+    $dateBefore = clone $date;
+    $dateBefore->sub(new DateInterval('P1D'));
+    $args["dateBefore"] = $dateBefore->format("Y-m-d");
+
+    $dateAfter = clone $date;
+    $dateAfter->add(new DateInterval('P1D'));
+    $args["dateAfter"] = $dateAfter->format("Y-m-d");
 
     // Appel au service
     $dataService = new DataService();
-    $data = $dataService->GetDataByDate($today);
+    $data = $dataService->GetDataByDate($date);
     $args = array_merge($args, $data);
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+// Méthode de validation de date
+function validateDate($date)
+{
+    $d = DateTime::createFromFormat('Y-m-d', $date);
+    return $d && $d->format('Y-m-d') === $date;
+}
